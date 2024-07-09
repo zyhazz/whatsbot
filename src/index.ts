6d4b446ea229622414ab79c4e77adc3906a79ac0
Diego Carvalho <zyhazz@gmail.com>
2024-07-08 22:45:57 -0300

    add vision

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ client.on('ready', () => {
     console.log('Cliente está pronto!');
 });
 
-client.on('message', (message) => {
+client.on('message', async (message) => {
     if (message.body.startsWith('!ping')) {
         message.reply('pong');
     }
@@ -30,10 +30,18 @@ client.on('message', (message) => {
     if (message.body.startsWith('!')) {
         //remove only the first ! from the message
         const text = message.body.slice(1);
-        askOllama(text).then((response) => {
-            console.log("resposta", response)
-            message.reply(response);
-        });
+        if (message.hasMedia) {
+            const media = await message.downloadMedia();
+            askLlava(text, media.data).then((response) => {
+                console.log("resposta", response)
+                message.reply(response);
+            });
+        }else {
+            askOllama(text).then((response) => {
+                console.log("resposta", response)
+                message.reply(response);
+            });
+        }
     }
     console.log("mensagem recebida", message.body);
 });
@@ -43,6 +51,17 @@ async function askOllama(question: string): Promise<string> {
         model: 'llama3',
         prompt: "Responda a questao a seguir em poucas palavras, em portugues do Brasil: " + question,
         stream: false,
+    });
+    return data.response ?? 'Não foi possível obter uma resposta';
+}
+
+async function askLlava(question: string, image: string): Promise<string> {
+    const {data} = await axios.post('http://localhost:11434/api/generate', {
+        model: 'llava',
+        //prompt: "Responda a questao a seguir em poucas palavras, em portugues do Brasil: " + question,
+        prompt: "O que esta acontecendo na imagem a seguir? " + question,
+        stream: false,
+        images: [image],
     });
     return data.response ?? 'Não foi possível obter uma resposta';
 }
